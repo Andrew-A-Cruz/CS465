5e14024af9bc6a2ecea162a3d959be1d65426fa0
Andrew-A-Cruz <techgeek1998@gmail.com>
2023-02-12 17:18:51 -0500

    modifed trips.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const ctrlMain = require('../controllers/main');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+const tripsController = require('../controllers/trips');
+
+router
+    .route('/trips')
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
+
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode);
 
 module.exports = router;
